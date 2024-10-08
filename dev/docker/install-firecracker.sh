f8776d35728d8b393705222076224921a7659d5c
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-10-08 18:36:11 -0400

    Add firecracker to the docker image

---
+++ dev/docker/install-firecracker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+ARCH="$(uname -m)"
+release_url="https://github.com/firecracker-microvm/firecracker/releases"
+
+version="v1.9.1"
+
+curl -L "${release_url}/download/${version}/firecracker-${version}-${ARCH}.tgz" | tar -xz
+mv release-${version}-$(uname -m)/firecracker-${version}-${ARCH} firecracker
+
