f79d99589ffc0df8795e079b1ce378722bba195c
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-12-07 19:16:30 -0500

    Basic run-site.sh

---
+++ orchestrator/orchestrator/api/docker/templates/run-site.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# This is the script used to run a site.
+# We look for a run.sh file in this script to avoid
+# rebuilding the docker image if it changes.
+
+date +'DIRECTOR: Starting server at %Y-%m-%d %H:%M:%S %Z'
+
+# This is filled in by the orchestrator
+paths="{paths}"
+
+for path in $paths; do
+    if [ -x "$path" ]; then
+        term() {
+            date +'DIRECTOR: Stopping server at %Y-%m-%d %H:%M:%S %Z'
+            kill "$child"
+        }
+        trap term TERM
+
+        "$path" &
+        child="$!"
+
+        while ! wait; do true; done
+        exec date +'DIRECTOR: Stopped server at %Y-%m-%d %H:%M:%S %Z'
+    fi
+done
+echo 'DIRECTOR: No run.sh file found -- if it exists, make sure it is set as executable'
+exec sleep infinity
