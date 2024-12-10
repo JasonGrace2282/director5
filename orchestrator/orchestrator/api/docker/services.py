667bb1bfe99f551cf5e473f610fba6ec5dadb6db
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-12-09 19:58:05 -0500

    mypy

--- orchestrator/orchestrator/api/docker/services.py
+++ orchestrator/orchestrator/api/docker/services.py
@@ -14,9 +14,9 @@ TEMPLATE_DIR = Path(__file__).parent / "templates"
 def create_service_params(client: docker.DockerClient, site_info: SiteInfo) -> dict[str, Any]:
     # default to looking through /site for a run.sh, for backwards compatibility
     if site_info.runfile is None:
-        runfiles = ("/site/run.sh", "/site/private/run.sh", "/site/public/run.sh")
+        runfiles = ["/site/run.sh", "/site/private/run.sh", "/site/public/run.sh"]
     else:
-        runfiles = (site_info.runfile,)
+        runfiles = [site_info.runfile]
     shell_cmd = (TEMPLATE_DIR / "run-site.sh").read_text()
     # the shlex.join function helps prevent shell injection
     shell_cmd = shell_cmd.replace("$SEARCH_PATH", shlex.join(runfiles))
