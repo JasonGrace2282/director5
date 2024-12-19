c2aa4baa28107e2c408b0def3ec86ded761c50bb
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-12-19 09:49:52 -0500

    Add some minimal tests

--- orchestrator/conftest.py
+++ orchestrator/conftest.py
@@ -1,7 +1,38 @@
+import pytest
 from fastapi.testclient import TestClient
 
+from orchestrator.api.docker.schema import SiteInfo
 from orchestrator.main import app
 
 
+@pytest.fixture
 def client() -> TestClient:
     return TestClient(app)
+
+
+@pytest.fixture
+def site_info() -> SiteInfo:
+    return SiteInfo(
+        pk=0,
+        hosts=["example.localhost.com"],
+        is_served=True,
+        resource_limits={"cpus": 1, "memory": "1MiB"},
+        docker={"base": "python:3.12-alpine"},
+    )
+
+
+@pytest.fixture
+def db_site_info() -> SiteInfo:
+    return SiteInfo(
+        pk=0,
+        hosts=["example.localhost.com"],
+        is_served=True,
+        resource_limits={"cpus": 1, "memory": "1MiB"},
+        docker={"base": "python:3.12-alpine"},
+        db={
+            "url": "mysql://user:password@localhost:3306/db",
+            "name": "db",
+            "username": "user",
+            "password": "password",
+        },
+    )
