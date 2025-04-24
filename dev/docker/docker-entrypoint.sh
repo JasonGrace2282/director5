6013ba5b4cf16606a33e2ad9d42f210439ca0d36
JasonGrace2282 <aarush.deshpande@gmail.com>
2025-04-23 22:35:16 -0400

    Reduce docker image space

--- dev/docker/docker-entrypoint.sh
+++ dev/docker/docker-entrypoint.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-uv sync
-
 if [ "$1" = "django" ]; then
-    python manage.py migrate --noinput
+    uv run python manage.py migrate --noinput
     # hand control over to django to handle SIGTERM
-    exec python manage.py runserver 0.0.0.0:8080
+    exec uv run python manage.py runserver 0.0.0.0:8080
 elif [ "$1" = "tailwind" ]; then
     static="manager/director/static/tailwind"
     exec dev/tailwind/tailwindcss -i $static/input.css -o $static/build.css --config dev/tailwind/tailwind.config.js --watch --poll
