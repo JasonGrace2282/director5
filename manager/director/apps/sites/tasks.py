4a23822507c9912eff790c0ad1e6c9aaf90808d7
JasonGrace2282 <aarush.deshpande@gmail.com>
2025-06-27 13:29:08 -0400

    Add rebuild docker image task

--- manager/director/apps/sites/tasks.py
+++ manager/director/apps/sites/tasks.py
@@ -24,6 +24,17 @@ def restart_site_process(operation_id: int) -> None:
 
 
 @shared_task
+def rebuild_docker_image(operation_id: int) -> None:
+    with auto_run_operation_wrapper(operation_id) as wrapper:
+        wrapper.register_action(
+            "Rebuilding Docker image",
+            actions.build_docker_image,
+            user_recoverable=True,
+        )
+        wrapper.register_action("Restarting Docker service", actions.update_docker_service)
+
+
+@shared_task
 def delete_site(operation_id: int) -> None:
     site = Site.objects.get(operation__id=operation_id)
 
